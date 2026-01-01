165013f584f8dcdaa7f2a23dbddda860f657c9f8
imhansai <imhansai@foxmail.com>
2026-01-01 09:39:40 +0800

    学习 JetBrains koog 记录1

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    id("java")
+    kotlin("jvm")
+}
+
+group = "dev.fromnowon"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation("ai.koog:koog-agents:0.6.0")
+    implementation("ch.qos.logback:logback-classic:1.5.21")
+
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(25)
+}
